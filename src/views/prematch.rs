5edbad78b400e0e52f3b8b1e53fb3dc9ee085751
Cameron McLaughlin <cronmclaughlin@gmail.com>
2025-09-24 01:39:48 -0400

    Move off_game_data to config/data structure

--- src/views/prematch.rs
+++ src/views/prematch.rs
@@ -1,20 +1,16 @@
 use dioxus::prelude::*;
 
-use crate::config::data::GLOBAL_DATA;
+use crate::config::data::{self, GLOBAL_DATA};
 use serde_json::Value;
-
-#[derive(Clone, Default)]
-struct OffGameData {
-    team_number: String,
-    match_number: String,
-}
 
 #[component]
 pub fn Prematch() -> Element {
-    let mut off_game_data = use_signal(|| OffGameData::default());
-
-    let team_number = off_game_data().team_number.clone();
-    let match_number = off_game_data().match_number.clone();
+    let mut off_data = use_signal(|| data::initialize_data());
+    
+    let off_data_read = off_data.read(); // Need to make sure this and below values are updated on input
+    
+    let team_number = off_data_read.get("prematch", "Match ID", "TN").unwrap();
+    let match_number = off_data_read.get("prematch", "Match ID", "MN").unwrap();
 
     rsx! {
         div { class: "container",
@@ -28,9 +24,9 @@ pub fn Prematch() -> Element {
                         value: "{team_number}",
                         placeholder: "Team Number",
                         oninput: move |evt| {
-                            let mut data = off_game_data().clone();
-                            data.team_number = evt.value().clone();
-                            off_game_data.set(data);
+                            let mut new_data = off_data();
+                            new_data.add("prematch", "Match ID", "TN", Value::String(evt.value().clone()));
+                            off_data.set(new_data);
                         },
                         r#type: "number"
                     }
@@ -45,33 +41,33 @@ pub fn Prematch() -> Element {
                         value: "{match_number}",
                         placeholder: "Match Number",
                         oninput: move |evt| {
-                            let mut data = off_game_data().clone();
-                            data.match_number = evt.value().clone();
-                            off_game_data.set(data);
+                            let mut new_data = off_data();
+                            new_data.add("prematch", "Match ID", "MN", Value::String(evt.value().clone()));
+                            off_data.set(new_data);
                         },
                         r#type: "number"
                     }
                 }
             }
 
-            if !team_number.is_empty() && !match_number.is_empty() {
+            if !team_number.to_string().is_empty() && !match_number.to_string().is_empty() {
                 div { class: "button-container",
                     button {
                         class: "subtitle-block",
                         onclick: move |_| {
-                            let current_data = off_game_data();
+                            let current_data = off_data();
 
                             GLOBAL_DATA.lock().unwrap().add(
                                 "prematch",
                                 "Match Info",
                                 "team_number",
-                                Value::String(current_data.team_number.clone()),
+                                Value::String(current_data.get("prematch", "Match ID", "TN").unwrap().to_string()),
                             );
                             GLOBAL_DATA.lock().unwrap().add(
                                 "prematch",
                                 "Match Info",
                                 "match_number",
-                                Value::String(current_data.match_number.clone()),
+                                Value::String(current_data.get("prematch", "Match ID", "TN").unwrap().to_string()),
                             );
                         },
                         "Submit Data"
