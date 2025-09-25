4bf54b975d1208695842c484330e21541a0c7354
nekhilsaxena <nekhil.saxena@gmail.com>
2025-09-25 15:29:49 -0400

    Yep
    RUST APP WORKS COUNTER 7

--- src/views/auton.rs
+++ src/views/auton.rs
@@ -1,38 +1,116 @@
-use dioxus::prelude::*;
-
 use crate::components::Counter;
 use crate::config::data::GLOBAL_DATA;
-
-const CSS: Asset = asset!("/assets/styling/auton.css");
+use dioxus::prelude::*;
+use serde_json::{Number, Value};
+const AUTON_CSS: Asset = asset!("/assets/styling/auton.css");
 
 #[component]
 pub fn Auton() -> Element {
-    let global_data = GLOBAL_DATA();
-    let auton_config = global_data.get_phase_data("auton").unwrap();
-    
+    let auton_data =
+        GLOBAL_DATA.with(|data| data.get_phase_data("auton").cloned().unwrap_or_default());
+
+    let mut local_auton_data = use_signal(|| auton_data.clone());
+
+    let mut is_submitted = use_signal(|| false);
+
     rsx! {
-        document::Link {rel: "stylesheet", href: CSS} 
-        
-        div {
-            div { class: "header", h1 { class: "header_text", "Auton" } }
-            for (category, items) in auton_config.iter() {
-                div { class: "section",
-                    h2 { "{category}" }
-                    for (item, value) in items.iter() {
-                        if  value.is_number() { 
-                            Counter {
-                                count: use_signal(|| value.as_i64().unwrap_or(0) as i32),
-                                title: item.clone()
-                            } 
-                        }
-                        else if value.is_boolean() { // Need to make way to update boolean
-                            button { "{item}" } // Currently returns item id
+        document::Link { rel: "stylesheet", href: AUTON_CSS }
+
+        div { class: "teleop-container",
+            div { class: "header-section",
+                div { class: "main-header",
+                    h1 { class: "title", "Auton" }
+                }
+                if is_submitted.read().to_owned() {
+                    div { class: "navigation",
+                        Link { class: "nav-btn prev", to: "/pages/teleop",
+                            i { class: "arrow right" }
+                            span { "Teleop" }
                         }
                     }
-                } 
-            } 
-            button { "Submit" }
-            link { "Teleop" }
+                } else {
+                    button {
+                        class: "nav-btn next disabled",
+                        disabled: true,
+                        span { "Teleop" }
+                        i { class: "arrow right" }
+                    }
+                }
+            }
+
+            div { class: "content-area",
+                if local_auton_data.read().is_empty() {
+                    div { class: "empty-state",
+                        i { class: "icon data" }
+                        p { "No teleop data categories configured" }
+                    }
+                } else {
+                    div { class: "counters-column",
+                        for (category, items) in local_auton_data.read().iter() {
+                            div { class: "category-section",
+                                div { class: "category-header",
+                                    h2 { class: "category-title", "{category}" }
+                                }
+                                div { class: "counters-list",
+                                    {items.iter().map(|(item, value)| {
+                                        let item_clone = item.clone();
+                                        let category_clone = category.clone();
+                                        let initial_count = value.as_i64().unwrap_or(0) as i32;
+
+                                        rsx! {
+                                            div { class: "counter-item",
+                                                Counter {
+                                                    count: use_signal(|| initial_count),
+                                                    title: item.clone(),
+                                                    on_change: move |new_count| {
+                                                        local_auton_data.with_mut(|data| {
+                                                            if let Some(category_data) = data.get_mut(&category_clone) {
+                                                                if let Some(value) = category_data.get_mut(&item_clone) {
+                                                                    *value = Value::Number(Number::from(new_count));
+                                                                }
+                                                            }
+                                                        });
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    })}
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            div { class: "action-bar",
+                button {
+                    class: "submit-btn primary",
+                    onclick: move |_| {
+                        GLOBAL_DATA.with_mut(|global_data| {
+                            let local_data = local_auton_data.read();
+                            for (category, items) in local_data.iter() {
+                                for (item, value) in items {
+                                    let count = value.as_i64().unwrap_or(0) as i32;
+                                    global_data.add("auton", category, item, Value::Number(Number::from(count)));
+                                }
+                            }
+                        });
+
+                        // Print the updated data
+                        GLOBAL_DATA.with(|data| {
+                            data.print_phase("prematch");
+                        });
+
+                        GLOBAL_DATA.with(|data| {
+                            data.print_phase("auton");
+                        });
+
+                        is_submitted.set(true);
+                    },
+                    i { class: "icon check" }
+                    span { "Submit Auton Data" }
+                }
+            }
         }
     }
 }
