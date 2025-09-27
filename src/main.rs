41478ab52021192ef83d04d542b01b727dbce5d2
nekhilsaxena <nekhil.saxena@gmail.com>
2025-09-27 02:01:13 -0400

    Attempted to apply chewy again didnt work

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use dioxus::prelude::*;
-
 use views::{Auton, Confirmation, Home, Postmatch, Prematch, Teleop};
 
 mod components;
@@ -31,6 +30,7 @@ enum Route {
 const FAVICON: Asset = asset!("/assets/images/BEAN.png");
 const MAIN_CSS: Asset = asset!("/assets/styling/main.css");
 const TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
+const CHEWY_FONT: Asset = asset!("/assets/fonts/Chewy-Regular.ttf");
 
 fn main() {
     dioxus::launch(App);
@@ -42,6 +42,14 @@ fn App() -> Element {
         document::Link { rel: "icon", href: FAVICON }
         document::Link { rel: "stylesheet", href: MAIN_CSS }
         document::Link { rel: "stylesheet", href: TAILWIND_CSS }
+
+        // Preload the font using a link tag with preload
+        document::Link {
+            rel: "preload",
+            href: CHEWY_FONT,
+            r#as: "font",
+            crossorigin: "anonymous"
+        }
 
         Router::<Route> {}
     }
